11e4e1fc5e8834816b5bd75c4a49e0fd457a18fe
yang <93424156+otomedic@users.noreply.github.com>
2022-12-10 21:18:07 +0900

    마이페이지 컨트롤러

--- src/main/java/com/esc/springcinema/controller/CinemaController.java
+++ src/main/java/com/esc/springcinema/controller/CinemaController.java
@@ -2,6 +2,8 @@ package com.esc.springcinema.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class CinemaController {
@@ -9,5 +11,11 @@ public class CinemaController {
     @RequestMapping("/")
     public String index() throws Exception {
         return "index";
+    }
+
+    @RequestMapping(value = "/mypage", method = RequestMethod.GET)
+    public ModelAndView openMyPage() throws Exception {
+        ModelAndView mv = new ModelAndView("mypage/mypage");
+        return mv;
     }
 }
