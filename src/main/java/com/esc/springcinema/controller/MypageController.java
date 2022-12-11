d49a7483af4c1b76474162b1620cf228a769fd7e
yang <93424156+otomedic@users.noreply.github.com>
2022-12-12 00:59:36 +0900

    마이페이지 전용 컨트롤러

--- src/main/java/com/esc/springcinema/controller/MypageController.java
+++ src/main/java/com/esc/springcinema/controller/MypageController.java
@@ -22,29 +22,4 @@ public class MypageController {
         ModelAndView mv = new ModelAndView("mypage/profile_update");
         return mv;
     }
-
-    @RequestMapping(value = "/seat" , method = RequestMethod.GET)
-    public ModelAndView openSeat() throws Exception {
-        ModelAndView mv = new ModelAndView("movieseat");
-
-        List<Integer> people = new ArrayList<>();
-        people.add(0);
-        people.add(1);
-        people.add(2);
-        people.add(3);
-        people.add(4);
-        people.add(5);
-
-        List<String> seatCode = new ArrayList<>();
-        seatCode.add("A");
-        seatCode.add("B");
-        seatCode.add("C");
-        seatCode.add("D");
-        seatCode.add("E");
-
-
-        mv.addObject("alp", seatCode);
-        mv.addObject("people", people);
-        return mv;
-    }
 }
