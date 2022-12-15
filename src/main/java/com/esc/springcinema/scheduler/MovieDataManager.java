d35cded1d8f9bf2d6765ad7a53e0258c6d169af9
MoonNight285 <65889750+MoonNight285@users.noreply.github.com>
2022-12-15 23:32:44 +0900

    특정시간에 영화목록 업데이트 기능 추가

--- src/main/java/com/esc/springcinema/scheduler/MovieDataManager.java
+++ src/main/java/com/esc/springcinema/scheduler/MovieDataManager.java
@@ -1,6 +1,7 @@
 package com.esc.springcinema.scheduler;
 
 import com.esc.springcinema.service.MovieService;
+import com.esc.springcinema.service.MovieServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
@@ -10,20 +11,25 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
+// 나중에 컨트롤러 제거해야함
+// 그리고 서버 켜질때 데이터 갱신해야함
 @Component
 @Controller
 public class MovieDataManager {
     @Autowired
     private MovieService movieService;
-    
+
+    // 새벽3시에 한번씩 영화목록을 업데이트 해준다.
+    // 작성일 : 2022-12-15
+    // 작성자 : MoonNight285
     @Scheduled(cron = "0 0 3 * * *")
-    public void test() {
-        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS");
-        Date now = new Date();
-        String strDate = sdf.format(now);
-        System.out.println("Java cron job expression:: " + strDate);
+    public void updateMovieInfo() throws Exception {
+        movieService.init();
     }
-    
+
+    // 테스트용도
+    // 작성일 : 2022-12-15
+    // 작성자 : MoonNight285
     @RequestMapping("/test")
     private String getMovieDatas() throws Exception {
         movieService.init();
