32597fd99e59c01ee310cc16b10fcc0e2e4e48eb
eblyncho <eblyn52@naver.com>
2022-12-17 17:58:50 +0900

    결제 전 예매정보 확인

--- src/main/java/com/esc/springcinema/controller/PaymentsController.java
+++ src/main/java/com/esc/springcinema/controller/PaymentsController.java
@@ -17,27 +17,36 @@ public class PaymentsController {
 
     @Autowired
     private PaymentService paymentService;
-
-    @RequestMapping(value = "/book", method = RequestMethod.POST)
-    public ModelAndView createMember(HttpServletRequest request) throws Exception {
-        PaymentsDto payment = new PaymentsDto();
-
-        boolean result = paymentService.paymentComplete(payment);
+    
+    // 결제 완료 후 페이지 이동
+    // 최종 수정 : 2022-12-17
+    // 마지막 작성자 : EblynCho
+    @RequestMapping(value = "/paymentComplete", method = RequestMethod.GET)
+    public ModelAndView paymentComplete(HttpServletRequest request) throws Exception {
+//        PaymentsDto payment = new PaymentsDto();
+//
+//        boolean result = paymentService.paymentComplete(payment);
         ModelAndView resultView = new ModelAndView("common/process_complete");
-        if (result == true) { // 아이디가 올바르게 들어갔는지 검증하는 기능이 아직 구현되지않음 무조건 true
+//        if (result == true) {
             resultView.addObject("title", "스프링 시네마 - 예매 완료");
             resultView.addObject("headMsg", "예매가 완료되었습니다.");
             resultView.addObject("link", "/main");
             resultView.addObject("btnMsg", "메인으로");
-        } else {
+//        }
+        return resultView;
+    }
+
+    // 결제 실패 후 페이지 이동
+    // 최종 수정 : 2022-12-17
+    // 마지막 작성자 : EblynCho
+    @RequestMapping(value = "/paymentCancel", method = RequestMethod.GET)
+    public ModelAndView paymentCancel(HttpServletRequest request) throws Exception {
+        ModelAndView resultView = new ModelAndView("common/process_complete");
             resultView.addObject("title", "스프링 시네마 - 예매 실패");
             resultView.addObject("headMsg", "예매 결제 도중 문제가 발생했습니다.");
             resultView.addObject("link", "/main");
             resultView.addObject("btnMsg", "메인으로");
-        }
-
         return resultView;
     }
-
 
 }
